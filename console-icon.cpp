c1a8d2547ee87e6d4a392df8d530052a8384e6d8
Ian Prest <github.com@ian.prest.ca>
2010-04-21 17:46:50 -0400

    fixed whitespace

--- console-icon.cpp
+++ console-icon.cpp
@@ -1,13 +1,13 @@
 /*SDOC**********************************************************************
 
-	Module:				console-icon.cpp
+  Module:       console-icon.cpp
 
-	Author:				Ian Prest
+  Author:       Ian Prest
 
-	Description:	Small utility to change the current console icon.
+  Description:  Small utility to change the current console icon.
 
-	Comments:			To build, simply run: 
-									cl.exe console-icon.cpp
+  Comments:     To build, simply run: 
+                  cl.exe console-icon.cpp
 
 ***********************************************************************EDOC*/
 #include <windows.h>
@@ -19,41 +19,41 @@
 // Main function
 int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 {
-	// Display a usage message if no parameter was specified
-	if(argc < 2)
-	{
-		_tprintf(_T("usage:\t\"%s\" - | <filename.ico>\n"), argv[0]);
-		return 1;
-	}
+  // Display a usage message if no parameter was specified
+  if(argc < 2)
+  {
+    _tprintf(_T("usage:\t\"%s\" - | <filename.ico>\n"), argv[0]);
+    return 1;
+  }
 
-	HICON hicon = NULL;
-	if(_tcscmp(argv[1], _T("-"))) 
-	{
-		// Load the icon from file
-		hicon = (HICON)::LoadImage(NULL, argv[1], IMAGE_ICON, 0, 0, LR_DEFAULTSIZE | LR_LOADFROMFILE);
-		if(!hicon) 
-		{
-			_tprintf(_T("error:\tcould not load icon \"%s\"\n"), argv[1]);
-			return 2;
-		}
-	}
+  HICON hicon = NULL;
+  if(_tcscmp(argv[1], _T("-"))) 
+  {
+    // Load the icon from file
+    hicon = (HICON)::LoadImage(NULL, argv[1], IMAGE_ICON, 0, 0, LR_DEFAULTSIZE | LR_LOADFROMFILE);
+    if(!hicon) 
+    {
+      _tprintf(_T("error:\tcould not load icon \"%s\"\n"), argv[1]);
+      return 2;
+    }
+  }
 
-	// Find the function in kernel32.dll
-	HMODULE hMod = LoadLibraryW(L"kernel32.dll");
-	typedef DWORD (__stdcall *_SetConsoleIcon)(HICON);
-	_SetConsoleIcon SetConsoleIcon = (_SetConsoleIcon)(GetProcAddress(hMod, "SetConsoleIcon"));
-	if(!SetConsoleIcon) 
-	{
-		_tprintf(_T("error:\tcould not access kernel32!SetConsoleIcon function\n"));
-		return 3;
-	}
+  // Find the function in kernel32.dll
+  HMODULE hMod = LoadLibraryW(L"kernel32.dll");
+  typedef DWORD (__stdcall *_SetConsoleIcon)(HICON);
+  _SetConsoleIcon SetConsoleIcon = (_SetConsoleIcon)(GetProcAddress(hMod, "SetConsoleIcon"));
+  if(!SetConsoleIcon) 
+  {
+    _tprintf(_T("error:\tcould not access kernel32!SetConsoleIcon function\n"));
+    return 3;
+  }
 
-	// Change the icon
-	SetConsoleIcon(hicon); 
+  // Change the icon
+  SetConsoleIcon(hicon); 
 
-	// Clean up
-	FreeLibrary(hMod);
-	return 0;
+  // Clean up
+  FreeLibrary(hMod);
+  return 0;
 }
 
 /*end of file*/
--- tcolor.cpp
+++ tcolor.cpp
@@ -1,14 +1,14 @@
 /*SDOC**********************************************************************
 
-	Module:				tcolor.cpp
+  Module:       tcolor.cpp
 
-	Author:				Ian Prest
+  Author:       Ian Prest
 
-	Description:	Small utility to change the current console text color.
-								Input values are the same as the built-in 'color' command.
+  Description:  Small utility to change the current console text color.
+                Input values are the same as the built-in 'color' command.
 
-	Comments:			To build, simply run: 
-									cl.exe tcolor.cpp
+  Comments:     To build, simply run: 
+                  cl.exe tcolor.cpp
 
 ***********************************************************************EDOC*/
 #include <windows.h>
@@ -18,36 +18,36 @@
 // Main function
 int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 {
-	// Display a usage message if no parameter was specified
-	if(argc < 2 || _tcslen(argv[1]) != 2)
-	{
-		_ftprintf(stderr, _T("usage:\t\"%s\" [bgc][fgc]\n"), argv[0]);
-		return -1;
-	}
+  // Display a usage message if no parameter was specified
+  if(argc < 2 || _tcslen(argv[1]) != 2)
+  {
+    _ftprintf(stderr, _T("usage:\t\"%s\" [bgc][fgc]\n"), argv[0]);
+    return -1;
+  }
 
-	// Get the color to use
-	WORD color = (WORD)_tcstol(argv[1], NULL, 16);
+  // Get the color to use
+  WORD color = (WORD)_tcstol(argv[1], NULL, 16);
 
-	// Open the output handle
-	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
-	if( INVALID_HANDLE_VALUE == hStdout )
-	{
-		_ftprintf(stderr, _T("error:\tcould not retrieve output handle\n"));
-		return -2;
-	}
+  // Open the output handle
+  HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+  if( INVALID_HANDLE_VALUE == hStdout )
+  {
+    _ftprintf(stderr, _T("error:\tcould not retrieve output handle\n"));
+    return -2;
+  }
 
-	// Get the current color
-	CONSOLE_SCREEN_BUFFER_INFO sbi = {};
-	GetConsoleScreenBufferInfo(hStdout, &sbi);
+  // Get the current color
+  CONSOLE_SCREEN_BUFFER_INFO sbi = {};
+  GetConsoleScreenBufferInfo(hStdout, &sbi);
 
-	// Change the color
-	if( color != 0x00 ) 
-	{
-		SetConsoleTextAttribute(hStdout, color);
-	}
+  // Change the color
+  if( color != 0x00 ) 
+  {
+    SetConsoleTextAttribute(hStdout, color);
+  }
 
-	// Return the old color
-	return sbi.wAttributes;
+  // Return the old color
+  return sbi.wAttributes;
 }
 
 /*end of file*/
